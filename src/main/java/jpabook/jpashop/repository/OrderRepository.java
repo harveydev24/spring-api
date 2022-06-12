609587299bb650c69f5d0cb9431ca6c0d1a2ad8a
harvey <harveydev24@gmail.com>
2022-06-13 00:41:39 +0900

    Implement collection optimization with paging

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -119,4 +119,14 @@ public class OrderRepository {
                                 " JOIN FETCH oi.item i", Order.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "SELECT o FROM Order o" +
+                        " JOIN FETCH o.member m" +
+                        " JOIN FETCH o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
